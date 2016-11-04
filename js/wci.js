40cfd0a405c981dbd684e85c7e22382e4c970112
Ben Calton <bac@pml.ac.uk>
2016-11-04 13:55:01 +0000

    more jiggery

---
+++ js/wci.js
@@ -0,0 +1,46 @@
+function goToByScroll(id) {
+   // Remove "link" from the ID
+   id = id.replace("link", "");
+   // Scroll
+   $('html,body').animate({
+         scrollTop: $("#" + id).offset().top - 150
+      },
+      'slow');
+}
+
+$("ul.navbar-nav > li > a[href='#']").click(function(e) {
+   e.preventDefault();
+   goToByScroll($(this).attr("id"));
+});
+
+$(".top-link").click(function(e) {
+   e.preventDefault();
+   goToByScroll('banner');
+});
+
+$(document).ready(function() {
+   $(".fancy").fancybox({
+      maxWidth : 800,
+      maxHeight   : 600,
+      fitToView   : false,
+      width    : '70%',
+      height      : '70%',
+      autoSize : false,
+      closeClick  : false,
+      openEffect  : 'none',
+      closeEffect : 'none',
+      padding  : 30
+   });
+   $(".fancy-text").fancybox({
+      fitToView   : false,
+      width    : '70%',
+      height      : '70%',
+      autoSize : false,
+      closeClick  : false,
+      openEffect  : 'none',
+      closeEffect : 'none',
+      padding  : 30,
+      type : 'iframe'
+   });
+
+});
